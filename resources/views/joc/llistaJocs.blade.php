bfaee3c1a218c3f57297f22f09dfdb9c57dda12a
oscar <oscar.camprubi@cirvianum.cat>
2022-06-21 19:42:01 +0200

    Commit Final Recus

--- resources/views/joc/llistaJocs.blade.php
+++ resources/views/joc/llistaJocs.blade.php
@@ -4,21 +4,23 @@
 @endsection
 @section('contingut')
     <h1 class="text-center mt-3">Llista de Jocs</h1>
-    @if($user->role == 1)
-    <a class="btn btn-success" href="/create-joc">Crea</a>
-    @endif
+    @isset($user)
+        @if($user->role == 1)
+            <a class="btn btn-success" href="/create-joc">Crea</a>
+        @endif
+    @endisset
     <div class="row m-3">
         @foreach($jocs as $joc)
-        <div class="col-9 card m-5" style="width: 18rem;">
-            <img src="{{$joc->image_path}}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{$joc->nom}}</h5>
-                <p class="card-text">{{$joc->descripcio}}</p>
+            <div class="col-9 card m-5" style="width: 18rem;">
+                <img src="{{$joc->image_path}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{$joc->nom}}</h5>
+                    <p class="card-text">{{$joc->descripcio}}</p>
+                </div>
+                <div class="card-footer">
+                    <a href="/show-joc/{{$joc->id}}" class="btn btn-primary">+ Info</a>
+                </div>
             </div>
-            <div class="card-footer">
-                <a href="/show-joc/{{$joc->id}}" class="btn btn-primary">+ Info</a>
-            </div>
-        </div>
         @endforeach
     </div>
 @endsection
